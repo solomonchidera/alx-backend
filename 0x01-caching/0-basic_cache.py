bf812b30cd9f77d600620e2bc804ec0bb2ae3158
solomonchidera <solochidera10@gmail.com>
2024-06-27 00:11:15 +0100

    chore: Update BasicCache class documentation formatting

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -4,13 +4,13 @@ BaseCaching = __import__("base_caching").BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """Class doc"""
+    """ Class doc """
 
     def put(self, key, item):
-        """Function doc"""
+        """ Function doc """
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        """Function doc"""
+        """ Function doc """
         return self.cache_data.get(key)

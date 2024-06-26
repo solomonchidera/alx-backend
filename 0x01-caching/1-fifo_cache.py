2cfcb3453ce5124cb37102d9e179ac32ef9196e8
solomonchidera <solochidera10@gmail.com>
2024-06-26 23:39:44 +0100

    class FIFOCache that inherits from BaseCaching and is a caching system

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+""" Module doc """
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ Class doc """
+
+    def __init__(self):
+        """ Function doc """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Function doc """
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                removed = next(iter(self.cache_data))
+                self.cache_data.pop(removed)
+                print("DISCARD: {}".format(removed))
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Function doc """
+        return self.cache_data.get(key)

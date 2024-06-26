60b657668c973d0feb63dc2322e496c714e96df3
solomonchidera <solochidera10@gmail.com>
2024-06-26 23:36:23 +0100

    class BasicCache that inherits from BaseCaching and is a caching system

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""Module doc"""
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Class doc"""
+
+    def put(self, key, item):
+        """Function doc"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Function doc"""
+        return self.cache_data.get(key)

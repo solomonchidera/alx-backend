633a97bb8de7a6624afe7b8b5314adf47ff36fa0
solomonchidera <solochidera10@gmail.com>
2024-06-26 23:44:04 +0100

    class LIFOCache that inherits from BaseCaching and is a caching system

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+""" module doc """
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """class doc"""
+
+    def __init__(self):
+        """function doc"""
+        super().__init__()
+
+    def put(self, key, item):
+        """function"""
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                removed = list(self.cache_data.keys())[-1]
+                self.cache_data.pop(removed)
+                print("DISCARD: {}".format(removed))
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """function doc"""
+        return self.cache_data.get(key)

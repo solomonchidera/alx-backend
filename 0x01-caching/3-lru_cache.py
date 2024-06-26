e649846a0f118b5340a0fb05b6e1dfbf7ba664f6
solomonchidera <solochidera10@gmail.com>
2024-06-26 23:56:26 +0100

    class LRUCache that inherits from BaseCaching and is a caching system

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python3
+""" module doc """
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """class doc"""
+
+    def __init__(self):
+        """func doc"""
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """func doc"""
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                removed = self.order.pop(0)
+                self.cache_data.pop(removed)
+                print("DISCARD: {}".format(removed))
+            self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key):
+        """func doc"""
+        if key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data.get(key)
